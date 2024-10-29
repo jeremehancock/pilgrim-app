7f9f47dd9bfff3435a38ecba7d22166ce217f965
Jereme <michael@jeremehancock.com>
2024-10-29 14:40:36 -0500

    renamed files

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,15 +4,15 @@ import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from '@/theme';
 
-export default function RootLayout(props: { children: React.ReactNode }) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
           <ThemeProvider theme={theme}>
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            {/* CssBaseline kickstarts an elegant, consistent, and simple baseline to build upon. */}
             <CssBaseline />
-            {props.children}
+            {children}
           </ThemeProvider>
         </AppRouterCacheProvider>
       </body>
