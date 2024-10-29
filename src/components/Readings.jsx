e3c2fa5d0c562a37b40300318f9cbd1f014cd7b0
Jereme <michael@jeremehancock.com>
2024-10-28 21:05:26 -0500

    mobile formatting

--- src/components/Readings.jsx
+++ src/components/Readings.jsx
@@ -19,31 +19,31 @@ const Readings = () => {
     // string = string
     // .replace(/<[^>]+>/g, '')
     return (
-      <div dangerouslySetInnerHTML={{ __html: string }} />
+      <span dangerouslySetInnerHTML={{ __html: string }} />
     );
   };
   
   return (
     <Stack>
-      <Typography sx={{ typography: { xs: 'h4', md: 'h1' }, mb: 5 }}>{data ? formatText(data.date) : '...'}</Typography>
-      <Typography sx={{ typography: { xs: 'h5', md: 'h2' }, mb: 5 }}>Today's Feast: {data ? formatText(data.day) : 'Loading...'}</Typography>
-      <Typography sx={{ typography: { xs: 'h5', md: 'h2' }, mb: 5 }}>First Reading</Typography>
-      <Typography sx={{ typography: { xs: 'h5', md: 'h3' }, mb: 3 }}>{data ? formatText(data.Mass_R1.source) : 'Loading...'}</Typography>
-      <Typography sx={{ typography: { xs: 'h5', md: 'h4' }, mb: 5 }}>{data ? formatText(data.Mass_R1.text) : 'Loading...'}</Typography>
-      <Typography sx={{ typography: { xs: 'h5', md: 'h2' }, mb: 5 }}>Today's Psalm</Typography>
-      <Typography sx={{ typography: { xs: 'h5', md: 'h3' }, mb: 3 }}>{data ? formatText(data.Mass_Ps.source) : 'Loading...'}</Typography>
-      <Typography sx={{ typography: { xs: 'h5', md: 'h4' }, mb: 5 }}>{data ? formatText(data.Mass_Ps.text) : 'Loading...'}</Typography>
-      {data?.Mass_R2 && (<Box><Typography sx={{ typography: { xs: 'h5', md: 'h2' }, mb: 5 }}>Second Reading</Typography>
-      <Typography sx={{ typography: { xs: 'h5', md: 'h3' }, mb: 3 }}>{data ? formatText(data.Mass_R2.source) : 'Loading...'}</Typography>
-      <Typography sx={{ typography: { xs: 'h5', md: 'h4' }, mb: 3 }}>{data ? formatText(data.Mass_R2.text) : 'Loading...'}</Typography></Box>)}
-      <Typography sx={{ typography: { xs: 'h5', md: 'h2' }, mb: 3 }}>Today's Gospel Accamation</Typography>
-      <Typography sx={{ typography: { xs: 'h5', md: 'h3' }, mb: 3 }}>{data ? formatText(data.Mass_GA.source) : 'Loading...'}</Typography>
-      <Typography sx={{ typography: { xs: 'h5', md: 'h4' }, mb: 5 }}>{data ? formatText(data.Mass_GA.text) : 'Loading...'}</Typography>
-      <Typography sx={{ typography: { xs: 'h5', md: 'h2' }, mb: 5 }}>Today's Gospel</Typography>
-      <Typography sx={{ typography: { xs: 'h5', md: 'h3' }, mb: 3 }}>{data ? formatText(data.Mass_G.source) : 'Loading...'}</Typography>
-      <Typography sx={{ typography: { xs: 'h5', md: 'h4' }, mb: 3  }}>{data ? formatText(data.Mass_G.text) : 'Loading...'}</Typography>
-      <Typography variant='label'>{data ? formatText(data.copyright.text) : '...'}</Typography>
-      {/* {data ? <pre>{JSON.stringify(data}</pre> : 'Loading...'} */}
+      <Typography sx={{ typography: { xs: 'h3', md: 'h1' }, mb: 5 }}>{data ? formatText(data.date) : ''}</Typography>
+      <Typography sx={{ typography: { xs: 'h4', md: 'h2' }, mb: 5 }}>Today's Feast: {data ? formatText(data.day) : ''}</Typography>
+      <Typography sx={{ typography: { xs: 'h3', md: 'h2' }, mb: 5 }}>First Reading</Typography>
+      <Typography sx={{ typography: { xs: 'h4', md: 'h3' }, mb: 3 }}>{data ? formatText(data.Mass_R1.source) : ''}</Typography>
+      <Typography sx={{ typography: { xs: 'h5', md: 'h4' }, mb: 5 }}>{data ? formatText(data.Mass_R1.text) : ''}</Typography>
+      <Typography sx={{ typography: { xs: 'h3', md: 'h2' }, mb: 5 }}>Today's Psalm</Typography>
+      <Typography sx={{ typography: { xs: 'h4', md: 'h3' }, mb: 3 }}>{data ? formatText(data.Mass_Ps.source) : ''}</Typography>
+      <Typography sx={{ typography: { xs: 'h5', md: 'h4' }, mb: 5 }}>{data ? formatText(data.Mass_Ps.text) : ''}</Typography>
+      {data?.Mass_R2 && (<Box><Typography sx={{ typography: { xs: 'h3', md: 'h2' }, mb: 5 }}>Second Reading</Typography>
+      <Typography sx={{ typography: { xs: 'h4', md: 'h3' }, mb: 3 }}>{data ? formatText(data.Mass_R2.source) : ''}</Typography>
+      <Typography sx={{ typography: { xs: 'h5', md: 'h4' }, mb: 3 }}>{data ? formatText(data.Mass_R2.text) : ''}</Typography></Box>)}
+      <Typography sx={{ typography: { xs: 'h3', md: 'h2' }, mb: 3 }}>Today's Gospel Accamation</Typography>
+      <Typography sx={{ typography: { xs: 'h4', md: 'h3' }, mb: 3 }}>{data ? formatText(data.Mass_GA.source) : ''}</Typography>
+      <Typography sx={{ typography: { xs: 'h5', md: 'h4' }, mb: 5 }}>{data ? formatText(data.Mass_GA.text) : ''}</Typography>
+      <Typography sx={{ typography: { xs: 'h3', md: 'h2' }, mb: 5 }}>Today's Gospel</Typography>
+      <Typography sx={{ typography: { xs: 'h4', md: 'h3' }, mb: 3 }}>{data ? formatText(data.Mass_G.source) : ''}</Typography>
+      <Typography sx={{ typography: { xs: 'h5', md: 'h4' }, mb: 3  }}>{data ? formatText(data.Mass_G.text) : ''}</Typography>
+      <Typography variant='label'>{data ? formatText(data.copyright.text) : ''}</Typography>
+      {/* {data ? <pre>{JSON.stringify(data}</pre> : 'Loading'} */}
     </Stack>
   );
 };
