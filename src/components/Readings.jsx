86294e6ff764fd2ffffc3018bd26ef1ae21b983f
Jereme <michael@jeremehancock.com>
2024-10-28 20:46:45 -0500

    Updated formating for mobile

--- src/components/Readings.jsx
+++ src/components/Readings.jsx
@@ -25,24 +25,23 @@ const Readings = () => {
   
   return (
     <Stack>
-      <Typography variant='h1' mb={5}>{data ? formatText(data.date) : '...'}</Typography>
-      <Typography variant='h2' mb={5}>Today's Feast: {data ? formatText(data.day) : 'Loading...'}</Typography>
-      <Typography variant='h3' mb={5}>First Reading</Typography>
-      <Typography variant='h4' mb={3}>{data ? formatText(data.Mass_R1.source) : 'Loading...'}</Typography>
-      <Typography variant='h4' mb={5}>{data ? formatText(data.Mass_R1.text) : 'Loading...'}</Typography>
-      <Typography variant='h3' mb={5}>Today's Psalm</Typography>
-      <Typography variant='h4' mb={3}>{data ? formatText(data.Mass_Ps.source) : 'Loading...'}</Typography>
-      <Typography variant='h4' mb={5}>{data ? formatText(data.Mass_Ps.text) : 'Loading...'}</Typography>
-      {data?.Mass_R2 && (<Box><Typography variant='h4' mb={5}>Second Reading</Typography>
-      <Typography variant='h4' mb={3}>{data ? formatText(data.Mass_R2.source) : 'Loading...'}</Typography>
-      <Typography variant='h4' mb={5}>{data ? formatText(data.Mass_R2.text) : 'Loading...'}</Typography></Box>)}
-      <Typography variant='h3' mb={5}>Today's Gospel Accamation</Typography>
-      <Typography variant='h4' mb={3}>{data ? formatText(data.Mass_GA.source) : 'Loading...'}</Typography>
-      <Typography variant='h4' mb={5}>{data ? formatText(data.Mass_GA.text) : 'Loading...'}</Typography>
-      <Typography variant='h3' mb={5}>Today's Gospel</Typography>
-      {/* <h2>{data ? formatText(data.Mass_G.heading) : 'Loading...'}</h2> */}
-      <Typography variant='h4' mb={3}>{data ? formatText(data.Mass_G.source) : 'Loading...'}</Typography>
-      <Typography variant='h4' mb={5}>{data ? formatText(data.Mass_G.text) : 'Loading...'}</Typography>
+      <Typography sx={{ typography: { xs: 'h4', md: 'h1' }, mb: 5 }}>{data ? formatText(data.date) : '...'}</Typography>
+      <Typography sx={{ typography: { xs: 'h5', md: 'h2' }, mb: 5 }}>Today's Feast: {data ? formatText(data.day) : 'Loading...'}</Typography>
+      <Typography sx={{ typography: { xs: 'h5', md: 'h2' }, mb: 5 }}>First Reading</Typography>
+      <Typography sx={{ typography: { xs: 'h5', md: 'h3' }, mb: 3 }}>{data ? formatText(data.Mass_R1.source) : 'Loading...'}</Typography>
+      <Typography sx={{ typography: { xs: 'h5', md: 'h4' }, mb: 5 }}>{data ? formatText(data.Mass_R1.text) : 'Loading...'}</Typography>
+      <Typography sx={{ typography: { xs: 'h5', md: 'h2' }, mb: 5 }}>Today's Psalm</Typography>
+      <Typography sx={{ typography: { xs: 'h5', md: 'h3' }, mb: 3 }}>{data ? formatText(data.Mass_Ps.source) : 'Loading...'}</Typography>
+      <Typography sx={{ typography: { xs: 'h5', md: 'h4' }, mb: 5 }}>{data ? formatText(data.Mass_Ps.text) : 'Loading...'}</Typography>
+      {data?.Mass_R2 && (<Box><Typography sx={{ typography: { xs: 'h5', md: 'h2' }, mb: 5 }}>Second Reading</Typography>
+      <Typography sx={{ typography: { xs: 'h5', md: 'h3' }, mb: 3 }}>{data ? formatText(data.Mass_R2.source) : 'Loading...'}</Typography>
+      <Typography sx={{ typography: { xs: 'h5', md: 'h4' }, mb: 3 }}>{data ? formatText(data.Mass_R2.text) : 'Loading...'}</Typography></Box>)}
+      <Typography sx={{ typography: { xs: 'h5', md: 'h2' }, mb: 3 }}>Today's Gospel Accamation</Typography>
+      <Typography sx={{ typography: { xs: 'h5', md: 'h3' }, mb: 3 }}>{data ? formatText(data.Mass_GA.source) : 'Loading...'}</Typography>
+      <Typography sx={{ typography: { xs: 'h5', md: 'h4' }, mb: 5 }}>{data ? formatText(data.Mass_GA.text) : 'Loading...'}</Typography>
+      <Typography sx={{ typography: { xs: 'h5', md: 'h2' }, mb: 5 }}>Today's Gospel</Typography>
+      <Typography sx={{ typography: { xs: 'h5', md: 'h3' }, mb: 3 }}>{data ? formatText(data.Mass_G.source) : 'Loading...'}</Typography>
+      <Typography sx={{ typography: { xs: 'h5', md: 'h4' }, mb: 3  }}>{data ? formatText(data.Mass_G.text) : 'Loading...'}</Typography>
       <Typography variant='label'>{data ? formatText(data.copyright.text) : '...'}</Typography>
       {/* {data ? <pre>{JSON.stringify(data}</pre> : 'Loading...'} */}
     </Stack>
