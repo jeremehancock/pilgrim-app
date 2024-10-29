3799fbbb7a0574e4b3e2bf1e7d859f84e7157879
Jereme <michael@jeremehancock.com>
2024-10-28 23:03:29 -0500

    more formatting

--- src/components/Readings.jsx
+++ src/components/Readings.jsx
@@ -22,6 +22,14 @@ const Readings = () => {
     );
   };
 
+  const addResponse = (string) => {
+    return string.replace(/<i>/g, '<i> <b>R:</b> ')
+  };
+
+  const addResponseToAlleluia = (string) => {
+    return string.replace(/Alleluia/g, '<b>R:</b> Alleluia')
+  };
+
   const removeIndent = (string) => {
     return string.replace(
       /(style="[^"]*)\btext-indent\s*:\s*[^;]+;?\s*/gi,
@@ -39,16 +47,16 @@ const Readings = () => {
       <Typography sx={{ typography: { xs: 'h3', md: 'h2' }, mb: 5 }}>First Reading</Typography>
       <Typography sx={{ typography: { xs: 'h4', md: 'h3' }, mb: 3 }}>{data ? formatText(data.Mass_R1.source) : ''}</Typography>
       <Typography sx={{ mb: 5 }}>{data ? formatText(data.Mass_R1.text) : ''}</Typography>
-      <Typography sx={{ typography: { xs: 'h3', md: 'h2' }, mb: 5 }}>Today's Psalm</Typography>
+      <Typography sx={{ typography: { xs: 'h3', md: 'h2' }, mb: 5 }}>Responsorial Psalm</Typography>
       <Typography sx={{ typography: { xs: 'h4', md: 'h3' }, mb: 3 }}>{data ? formatText(data.Mass_Ps.source) : ''}</Typography>
-      <Typography sx={{ mb: 5 }}>{data ? formatText(removeIndent(data.Mass_Ps.text)) : ''}</Typography>
+      <Typography sx={{ mb: 5 }}>{data ? formatText(addResponse(removeIndent(data.Mass_Ps.text))) : ''}</Typography>
       {data?.Mass_R2 && (<Box><Typography sx={{ typography: { xs: 'h3', md: 'h2' }, mb: 5 }}>Second Reading</Typography>
       <Typography sx={{ typography: { xs: 'h4', md: 'h3' }, mb: 3 }}>{data ? formatText(data.Mass_R2.source) : ''}</Typography>
       <Typography sx={{ mb: 3 }}>{data ? formatText(data.Mass_R2.text) : ''}</Typography></Box>)}
-      <Typography sx={{ typography: { xs: 'h3', md: 'h2' }, mb: 5 }}>Today's Gospel Acclamation</Typography>
+      <Typography sx={{ typography: { xs: 'h3', md: 'h2' }, mb: 5 }}>Alleluia</Typography>
       <Typography sx={{ typography: { xs: 'h4', md: 'h3' }, mb: 3 }}>{data ? formatText(data.Mass_GA.source) : ''}</Typography>
-      <Typography sx={{ mb: 5 }}>{data ? formatText(removeIndent(data.Mass_GA.text)) : ''}</Typography>
-      <Typography sx={{ typography: { xs: 'h3', md: 'h2' }, mb: 5 }}>Today's Gospel</Typography>
+      <Typography sx={{ mb: 5 }}>{data ? formatText(addResponseToAlleluia(removeIndent(data.Mass_GA.text))) : ''}</Typography>
+      <Typography sx={{ typography: { xs: 'h3', md: 'h2' }, mb: 5 }}>Gospel</Typography>
       <Typography sx={{ typography: { xs: 'h4', md: 'h3' }, mb: 3 }}>{data ? formatText(data.Mass_G.source) : ''}</Typography>
       <Typography sx={{ mb: 3  }}>{data ? formatText(data.Mass_G.text) : ''}</Typography>
       <Typography variant='body2'>{data ? formatText(data.copyright.text) : ''}</Typography>
