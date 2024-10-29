c7e03a3d9f002f97d1c38d552985071cccc35611
Jereme <michael@jeremehancock.com>
2024-10-29 09:37:02 -0500

    cleaned up functions

--- src/components/Readings.jsx
+++ src/components/Readings.jsx
@@ -16,22 +16,9 @@ const Readings = () => {
   }, []);
 
   const formatText = (string) => {
-    string = string.replace(/&#160;/g, '');
-    return (
-      <span dangerouslySetInnerHTML={{ __html: string }} />
-    );
-  };
-
-  const addResponse = (string) => {
-    return string.replace(/<i>/g, '<i> <b>R:</b> ')
-  };
-
-  const addResponseToAlleluia = (string) => {
-    return string.replace(/Alleluia/g, '<b>R:</b> Alleluia')
-  };
-
-  const removeFormatting = (string) => {
-    return string.replace(
+    string = string
+    .replace(/&#160;/g, '')
+    .replace(
       /(style="[^"]*)\btext-indent\s*:\s*[^;]+;?\s*/gi,
       '$1'
     ).replace(/style="\s*"/gi, 'style=""').replace(/\s*style=""/gi, '').replace(
@@ -47,28 +34,39 @@ const Readings = () => {
       /(style="[^"]*)\bcolor\s*:\s*[^;]+;?\s*/gi,
       '$1'
     ).replace(/style="\s*"/gi, 'style=""').replace(/\s*style=""/gi, '')
+    return (
+      <span dangerouslySetInnerHTML={{ __html: string }} />
+    );
+  };
+
+  const addResponse = (string) => {
+    return string.replace(/<i>/g, '<i> <b>R:</b> ')
+  };
+
+  const addResponseToAlleluia = (string) => {
+    return string.replace(/Alleluia/g, '<b>R:</b> Alleluia')
   };
   
   return (
     data ? (<Stack>
       <Typography sx={{ typography: { xs: 'h3', md: 'h1' }, mb: 5 }}>{data ? formatText(data.date) : ''}</Typography>
-      <Typography sx={{ typography: { xs: 'h4', md: 'h2' }, mb: 5 }}>{data ? formatText(removeFormatting(data.day)) : ''}</Typography>
+      <Typography sx={{ typography: { xs: 'h4', md: 'h2' }, mb: 5 }}>{data ? formatText(data.day) : ''}</Typography>
       <Typography sx={{ typography: { xs: 'h3', md: 'h2' }, mb: 5 }}>First Reading</Typography>
       <Typography sx={{ typography: { xs: 'h4', md: 'h3' }, mb: 3 }}>{data ? formatText(data.Mass_R1.source) : ''}</Typography>
       <Typography sx={{ mb: 5 }}>{data ? formatText(data.Mass_R1.text) : ''}</Typography>
       <Typography sx={{ typography: { xs: 'h3', md: 'h2' }, mb: 5 }}>Responsorial Psalm</Typography>
       <Typography sx={{ typography: { xs: 'h4', md: 'h3' }, mb: 3 }}>{data ? formatText(data.Mass_Ps.source) : ''}</Typography>
-      <Typography sx={{ mb: 5 }}>{data ? formatText(addResponse(removeFormatting(data.Mass_Ps.text))) : ''}</Typography>
+      <Typography sx={{ mb: 5 }}>{data ? formatText(addResponse(data.Mass_Ps.text)) : ''}</Typography>
       {data?.Mass_R2 && (<Box><Typography sx={{ typography: { xs: 'h3', md: 'h2' }, mb: 5 }}>Second Reading</Typography>
       <Typography sx={{ typography: { xs: 'h4', md: 'h3' }, mb: 3 }}>{data ? formatText(data.Mass_R2.source) : ''}</Typography>
       <Typography sx={{ mb: 3 }}>{data ? formatText(data.Mass_R2.text) : ''}</Typography></Box>)}
       <Typography sx={{ typography: { xs: 'h3', md: 'h2' }, mb: 5 }}>Alleluia</Typography>
       <Typography sx={{ typography: { xs: 'h4', md: 'h3' }, mb: 3 }}>{data ? formatText(data.Mass_GA.source) : ''}</Typography>
-      <Typography sx={{ mb: 5 }}>{data ? formatText(addResponseToAlleluia(removeFormatting(data.Mass_GA.text))) : ''}</Typography>
+      <Typography sx={{ mb: 5 }}>{data ? formatText(addResponseToAlleluia(data.Mass_GA.text)) : ''}</Typography>
       <Typography sx={{ typography: { xs: 'h3', md: 'h2' }, mb: 5 }}>Gospel</Typography>
       <Typography sx={{ typography: { xs: 'h4', md: 'h3' }, mb: 3 }}>{data ? formatText(data.Mass_G.source) : ''}</Typography>
       <Typography sx={{ mb: 10  }}>{data ? formatText(data.Mass_G.text) : ''}</Typography>
-      <Typography variant='label' fontSize={11}>{data ? formatText(removeFormatting(data.copyright.text)) : ''}</Typography>
+      <Typography variant='label' fontSize={11}>{data ? formatText(data.copyright.text) : ''}</Typography>
     </Stack>) : ''
   );
 };
