ca3b828cea45aef59763dac88446f24a78fc071a
Jereme <michael@jeremehancock.com>
2024-10-29 12:05:37 -0500

    trying to setup prettier/eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import NextLink from 'next/link';
-import ProTip from '@/components/ProTip';
-import Copyright from '@/components/Copyright';
 import Readings from '@/components/Readings';
 
 export default function Home() {
